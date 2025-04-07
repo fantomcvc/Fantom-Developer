7bf5b80c6165134a449f30d610e2c36731a8a071
Fantom <devrift@example.com>
2025-04-07 21:27:41 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Fantom-Developer",
+  base: "/",
   plugins: [
     react(),
     mode === 'development' &&
